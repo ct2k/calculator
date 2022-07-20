258d77e641d197aaf92941fb9a5a2d5a3957cc3d
ct2k <voicesfromthedarkside@protonmail.com>
2022-07-19 17:15:58 -0700

    Make calculator buttons clickable and store the input value into a variable

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 'use strict';
 
+// Selecting elements
+
+const displayEle = document.querySelector('.display');
+const inputEle = document.querySelector('.input');
+
+const numberEles = Array.from(
+  document.querySelectorAll("[class^='number'], [class^='operator'")
+);
+
+console.log(numberEles);
+
+numberEles.forEach(button => {
+  button.addEventListener('click', function () {
+    calculation = inputEle.textContent += button.textContent;
+  });
+});
+
 // Basic math functions
 
 function add(num1, num2) {
@@ -21,3 +38,7 @@ function divide(num1, num2) {
 function operate(operator, num1, num2) {
   return operator(num1, num2);
 }
+
+// Variables
+
+let calculation = 0;
